10db83494a4d49063f76fbf330f1971d477fc056
sunchengzhu <sunchengzhu945@gmail.com>
2023-08-02 17:36:46 +0800

    add forcerelay_devnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,9 +33,14 @@ const config: HardhatUserConfig = {
     },
     axon_alphanet: {
       url: "https://rpc-alphanet-axon.ckbapp.dev/",
-      accounts: [ "0x383fcff8683b8115e31613949be24254b4204ffbe43c227408a76334a2e3fb32"],
+      accounts: ["0x383fcff8683b8115e31613949be24254b4204ffbe43c227408a76334a2e3fb32"],
       chainId: 2022,
-    }
+    },
+    forcerelay_devnet: {
+      url: "http://54.238.73.76:8000",
+      accounts: ["0x383fcff8683b8115e31613949be24254b4204ffbe43c227408a76334a2e3fb32"],
+      chainId: 2022,
+    },
   }
 };
 
