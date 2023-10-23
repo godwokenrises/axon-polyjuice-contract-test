053d08f93a5b91a7e68807dfaf3ddfbc1d89cab5
sunchengzhu <36075573+sunchengzhu@users.noreply.github.com>
2023-10-23 17:17:26 +0800

    update chain id (#4)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,23 +24,23 @@ const config: HardhatUserConfig = {
     axon_local_devnet: {
       url: "http://127.0.0.1:8000",
       accounts: ["0x37aa0f893d05914a4def0460c0a984d3611546cfb26924d7a7ca6e0db9950a2d"],
-      chainId: 2022,
+      chainId: 1098411886,
     },
     axon_devnet: {
       url: "http://34.216.103.183:8000/",
       // 0xf386573563c3a75dbbd269fce9782620826ddac2
       accounts: ["0x383fcff8683b8115e31613949be24254b4204ffbe43c227408a76334a2e3fb32"],
-      chainId: 2022,
+      chainId: 1098411886,
     },
     axon_alphanet: {
       url: "https://rpc-alphanet-axon.ckbapp.dev/",
       accounts: ["0x383fcff8683b8115e31613949be24254b4204ffbe43c227408a76334a2e3fb32"],
-      chainId: 2022,
+      chainId: 1098411886,
     },
     forcerelay_devnet: {
       url: "http://54.238.73.76:8000",
       accounts: ["0x383fcff8683b8115e31613949be24254b4204ffbe43c227408a76334a2e3fb32"],
-      chainId: 2022,
+      chainId: 1098411886,
     },
   }
 };
